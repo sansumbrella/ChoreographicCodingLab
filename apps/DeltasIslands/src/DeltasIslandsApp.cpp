a1cb976b80607fafc77a5761ce6dd6b9e94ab7f2
David Wicks <wicks.db@gmail.com>
2015-08-28 08:25:54 -0400

    Stub application for CCL 4.
    Using master branch of Cinder and the blocks I cloned into it.

---
+++ apps/DeltasIslands/src/DeltasIslandsApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class DeltasIslandsApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void DeltasIslandsApp::setup()
+{
+}
+
+void DeltasIslandsApp::mouseDown( MouseEvent event )
+{
+}
+
+void DeltasIslandsApp::update()
+{
+}
+
+void DeltasIslandsApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP( DeltasIslandsApp, RendererGl )
